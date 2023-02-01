6e8f26e1bf327bb8f02ec7261acead93f0040a29
Mohammad Asaadi <67.asadi@gmail.com>
2023-02-01 23:36:45 +0330

    responsive

--- src/Components/Slideshow/Slideshow.jsx
+++ src/Components/Slideshow/Slideshow.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import moon from '../../images/moon.jpeg'
-import '../../styles/slideShow.css'
+import React from "react";
+import moon from "../../images/moon.jpeg";
+import "../../styles/slideShow.css";
 
 const Slideshow = () => {
-  return (
-    <div className='slideShow container-fluid'>
-      <div className='img'>
-        <img src={moon} alt="moon" />
-        <div className='overlay'></div>
-      </div>
-        <div className="text">
-          <span className='txt1'>We Build</span>
-          <span className='txt2'>Metaverse And</span>
-          <span className='txt3'>Blockchain Games</span>
-          <span className='txt4'>For You</span>
+    return (
+        <div className="slideShow container-fluid">
+            <div className="img">
+                <img src={moon} alt="moon" />
+                <div className="text">
+                    <span className="txt1">We Build</span>
+                    <span className="txt2">Metaverse And</span>
+                    <span className="txt3">Blockchain Games</span>
+                    <span className="txt4">For You</span>
+                </div>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Slideshow
+export default Slideshow;
--- src/Components/WhatToDo/WhatToDo.jsx
+++ src/Components/WhatToDo/WhatToDo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ".././../styles/whatToDo.css";
+import "../../styles/whatToDo.css";
 
 const WhatToDo = () => {
     return (
